550f544ea8c47833af7efdcd73ba05e0ca47b287
aPannell064 <157420768+aPannell064@users.noreply.github.com>
2025-05-09 08:33:49 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
-function fibTR(n, old=1, current=1, next) {
+function fibTR(n, old=1, current=1) {
     if(n <= 2) {return current;}
-    next = current + old;
+    var next = current + old;
     old = current;
     current = next;
-    return fibTR(n-1, old, current, next);
+    return fibTR(n-1, old, current);
 }
